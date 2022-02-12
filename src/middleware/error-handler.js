7f9b6dd23f1610817330a2522ee60e609365e27e
Shailesh Aanand <anaandshailu@gmail.com>
2022-02-12 23:17:15 +0530

    disable false error

--- src/middleware/error-handler.js
+++ src/middleware/error-handler.js
@@ -1,5 +1,6 @@
 const { StatusCodes } = require("http-status-codes");
 const CustomAPIError = require("../errors/custom-api");
+/* eslint-disable no-unused-vars */
 const errorHandlerMiddleware = (err, req, res, next) => {
   let customError = {
     statusCode: err.statusCode || StatusCodes.INTERNAL_SERVER_ERROR,
