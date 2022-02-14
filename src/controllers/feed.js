e390164e17da23808491f302cff89713c683bec7
Shailesh Aanand <anaandshailu@gmail.com>
2022-02-15 00:58:29 +0530

    fix bug in feed sorting

--- src/controllers/feed.js
+++ src/controllers/feed.js
@@ -2,10 +2,10 @@ const Post = require("../models/Post");
 
 const feed = async (req, res) => {
   const { following } = req.user;
-  const sortByLikes = !!req.query.sortByLikes;
+  const sortByLikes = req.query.sortByLikes === "true" ? true : false;
+  console.log("SORRRTTTT", { sortByLikes });
   const page = req.query.page || 1;
   const limit = req.query.limit || 10;
-  console.log({ sortByLikes });
   const posts = await Post.find({ author: { $in: following } })
     .sort(sortByLikes ? { likes: -1, createdAt: -1 } : { createdAt: -1 })
     .limit(limit)
