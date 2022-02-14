a736cfbc687bbd969c9848f493b0008a73a02a51
Shailesh Aanand <anaandshailu@gmail.com>
2022-02-15 00:58:53 +0530

    return user object directly

--- src/controllers/me.js
+++ src/controllers/me.js
@@ -1,21 +1,22 @@
 const { StatusCodes } = require("http-status-codes");
 
 const getMe = async (req, res) => {
-  res.status(StatusCodes.OK).json({ user: req.user });
+  res.status(StatusCodes.OK).json(req.user);
 };
 
 const updateMe = async (req, res) => {
-  const { first_name, last_name } = req.body;
+  const { first_name, last_name, password } = req.body;
   if (first_name) {
     req.user.first_name = first_name;
   }
   if (last_name) {
     req.user.last_name = last_name;
   }
+  if (password) {
+    req.user.password = password;
+  }
   req.user.save();
-  res.status(StatusCodes.OK).json({
-    user: req.user,
-  });
+  res.status(StatusCodes.OK).json(req.user);
 };
 
 module.exports = { getMe, updateMe };
